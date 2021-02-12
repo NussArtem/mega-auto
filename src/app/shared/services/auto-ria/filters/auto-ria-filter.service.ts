7b456307acc2ee11dbd96055f27d60d44b6a4da5
NussArtem <artem_nuss@mail.ru>
2021-02-12 12:32:34 +0200

    ADD

--- src/app/shared/services/auto-ria/filters/auto-ria-filter.service.ts
+++ src/app/shared/services/auto-ria/filters/auto-ria-filter.service.ts
@@ -2,8 +2,7 @@ import {Injectable} from '@angular/core';
 import {HttpClient, HttpParams} from '@angular/common/http';
 import {FilterParameters} from '@app/shared/models/interfaces/FilterParameters';
 import {AutoRiaAds} from '@app/shared/models/auto-ria-ads.model';
-import {FilterParametersGroupExtended} from "@app/shared/models/interfaces/helpers/filter-parameters-group-extended";
-import {FilterParameterGroupValue} from "@app/shared/models/interfaces/helpers/filter-parameter-group-value.model";
+import {FilterParameterGroupValue} from '@app/shared/models/interfaces/helpers/filter-parameter-group-value.model';
 
 @Injectable({
   providedIn: 'root'
@@ -21,13 +20,13 @@ export class AutoRiaFilterService {
     (`${this.apiUrl}/constant_parameters/`);
   }
 
-  getMarks(transportType: FilterParameters) {
+  getBrands(transportType: FilterParameters) {
     const params = new HttpParams().set('transport_type', String(transportType.value));
     return this.http.get<any>(`${this.apiUrl}/mark_parameters/`, {params});
   }
 
-  getModels(transportType: FilterParameters, markParameter: FilterParameters) {
-    const params = new HttpParams().set('transport_type', String(transportType.value)).set('mark', String(markParameter.value));
+  getModels(transportType: FilterParameters, brandParameter: FilterParameters) {
+    const params = new HttpParams().set('transport_type', String(transportType.value)).set('mark', String(brandParameter.value));
     const paramsTwo = new HttpParams();
     return this.http.get<any>
     (`${this.apiUrl}/model_parameters/`, {params});
@@ -39,7 +38,7 @@ export class AutoRiaFilterService {
 
   search(
     transportType: FilterParameters,
-    marks: FilterParameters[],
+    brands: FilterParameters[],
     models: FilterParameterGroupValue[],
     yearFrom: number,
     yearTo: number,
@@ -52,33 +51,30 @@ export class AutoRiaFilterService {
     mileageTo: number,
     volumeFrom: number,
     volumeTo: number,
-    page: number
+    page: number,
+    countOnPage: number
   ) {
     let params = new HttpParams();
     if (transportType) {
       params = params.append('category_id', String(transportType.value));
     }
-
+    if (brands && brands.length > 0) {
+      /*      params = params.append('marka_id', String(brands[0].value));*/
+      brands.forEach((brand, index) => {
+        params = params.append('marka_id[' + index + ']', String(brand.value));
+      });
+    }
     if (models && models.length > 0) {
-      console.log(models);
       models.forEach((model, index) => {
         params = params.append('marka_id[' + index + ']', String(model.parentalValue));
         params = params.append('model_id[' + index + ']', String(model.filterParameters.value));
       });
-    }else{
-      if (marks && marks.length > 0) {
-        console.log(marks);
-        /*      params = params.append('marka_id', String(marks[0].value));*/
-        marks.forEach((mark, index) => {
-          params = params.append('marka_id[' + index + ']', String(mark.value));
-        });
-      }
     }
     if (yearFrom) {
-      params = params.append('s_yers[0]', String(yearFrom));
+      params = params.append('s_yers', String(yearFrom));
     }
     if (yearTo) {
-      params = params.append('po_yers[0]', String(yearTo));
+      params = params.append('po_yers', String(yearTo));
     }
     if (regions && regions.length > 0) {
       regions.forEach((region, index) => {
@@ -102,10 +98,10 @@ export class AutoRiaFilterService {
       });
     }
     if (mileageFrom) {
-
+      params = params.append('mileage.gte', String(mileageFrom));
     }
     if (mileageTo) {
-
+      params = params.append('mileage.lte', String(mileageTo));
     }
     if (volumeFrom) {
       params = params.append('engineVolumeFrom', String(volumeFrom));
@@ -115,7 +111,7 @@ export class AutoRiaFilterService {
     }
 
     params = params.append('page', String(page));
-    console.log(params);
+    params = params.append('countpage', String(countOnPage));
     return this.http.get<AutoRiaAds>(`${this.apiUrl}/search/`, {params});
 
   }
